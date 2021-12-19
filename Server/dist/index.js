f12199583ecb857ccada2fb2be1f9cb582e0ae72
marcelhfm <hofmann.marcel@hm.edu>
2021-12-19 15:50:26 +0100

    all

---
+++ Server/dist/index.js
@@ -0,0 +1,68 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const core_1 = require("@mikro-orm/core");
+const express_1 = __importDefault(require("express"));
+const constants_1 = require("./constants");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
+const user_1 = require("./resolvers/user");
+const express_session_1 = __importDefault(require("express-session"));
+const apollo_server_core_1 = require("apollo-server-core");
+const cors_1 = __importDefault(require("cors"));
+const redis = require("redis");
+const main = async () => {
+    const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
+    await orm.getMigrator().up();
+    const app = (0, express_1.default)();
+    const RedisStore = require("connect-redis")(express_session_1.default);
+    const redisClient = redis.createClient();
+    app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
+    app.use((0, express_session_1.default)({
+        name: constants_1.COOKIE_NAME,
+        store: new RedisStore({
+            host: "localhost",
+            port: 6379,
+            client: redisClient,
+            disableTouch: true,
+        }),
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+            httpOnly: true,
+            sameSite: "lax",
+            secure: constants_1.__prod__,
+        },
+        secret: "keyboard cat",
+        resave: false,
+    }));
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        plugins: [(0, apollo_server_core_1.ApolloServerPluginLandingPageGraphQLPlayground)()],
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.HelloResolver, post_1.PostResolver, user_1.UserResolver],
+            validate: false,
+        }),
+        context: ({ req, res }) => ({ em: orm.em, req, res }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
+    app.get("/", (_, res) => {
+        res.send("Hello");
+    });
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000");
+    });
+};
+main().catch((err) => {
+    console.error(err);
+});
+//# sourceMappingURL=index.js.map
