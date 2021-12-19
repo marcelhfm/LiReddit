d0ce65b6b2a81a3937e5fa35f593d1c2115711e0
marcelhfm <hofmann.marcel@hm.edu>
2021-12-19 16:00:03 +0100

    all

---
+++ lireddit-web/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { withUrqlClient } from "next-urql";
+import { NavBar } from "../components/NavBar";
+import { createUrqlClient } from "../utils/createUrqlClient";
+
+const Index = () => (
+  <>
+    <NavBar />
+  </>
+);
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
